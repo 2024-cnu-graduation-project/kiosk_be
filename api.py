b8fc69609e4f786b7bc4ef5a22a44d6dffa64425
EunKyung Lee <2dmsrud2002@naver.com>
2024-05-22 14:18:59 +0900

    Feat: stt api 수정
    post로 파일을 받아와 내부 file 디렉토리에 저장 후, 위스퍼로 처리한다.

--- api.py
+++ api.py
@@ -1,22 +1,40 @@
 from openai import OpenAI
-from flask import Flask, render_template, request, jsonify
+import openai
+from flask import Flask, request, jsonify
 from flask_cors import CORS
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
 app = Flask(__name__)
 CORS(app)
 
+
 client = OpenAI()
 
-@app.route('/api/stt', methods=['GET'])
+@app.route('/api/stt', methods=['POST'])
 def stt():
-    audio_file = open("data/TEST9.mp3", "rb")
-    # audio_file = request.args.get('audio_file')
-    transcription = client.audio.transcriptions.create(
-        model = "whisper-1",
-        file=audio_file
-    )
-    print(transcription.text)
-    return jsonify({"text": transcription.text})
+    print('요청 들어옴')
+    if 'audio' not in request.files:
+        return jsonify({"error": "No audio file part"}), 400
+    audio_file = request.files['audio']
+    audio_file.save('./file/' + audio_file.filename)
+    print(audio_file)
+    files = open("file/recording.mp3", "rb")
+    try:
+        transcription = client.audio.transcriptions.create(
+            model = "whisper-1",
+            file = files
+        )
+        print(transcription.text)
+        response = jsonify({"text": transcription.text})
+        response.headers.add('Access-Control-Allow-Origin', '*')
+
+        return response
+    except Exception as e:
+        return jsonify({'error': str(e)}, 500)
 
 
 if __name__ == "__main__":
